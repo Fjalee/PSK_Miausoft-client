2794ec1f98b6c6688b30fc86bf26e8f4d9628cfa
Bertašius Paulius <paulius.bertasius@mif.stud.vu.lt>
2022-05-02 00:29:22 +0300

    Removed redundant line

--- src/pages/DeliveryPlan.js
+++ src/pages/DeliveryPlan.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getParcel, createDeliveryPlan } from '../services/ParcelsService';
